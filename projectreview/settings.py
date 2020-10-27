a9e03557bf198ed48e9b0362f49d21517f69a5e0
eugenek45 <eugenekiprotich0@gmail.com>
2020-10-27 23:11:37 +0300

    changed

--- projectreview/settings.py
+++ projectreview/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = 'projectreview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'review',
-        'USER': 'postgres',
-        'PASSWORD':'eugene',
+        'NAME':'',
+        'USER': '',
+        'PASSWORD':'',
     }
 }
 
