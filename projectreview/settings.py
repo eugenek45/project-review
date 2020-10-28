7117d21cb714322ff19ae8492645f3bc3c40c818
eugenek45 <eugenekiprotich0@gmail.com>
2020-10-28 12:39:54 +0300

    changed

--- projectreview/settings.py
+++ projectreview/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['awarrdskip.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'main',
     'accounts',
     'django.contrib.admin',
